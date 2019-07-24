fb5b7a3094500c026a2865cd9748a2cfa95d7ba7
amitkayal <amitkayal@outlook.com>
2019-07-24 18:14:51 +0530

    Update clr.py

--- clr.py
+++ clr.py
@@ -9,13 +9,13 @@ from keras import backend as K
 # Code is ported from https://github.com/fastai/fastai
 class OneCycleLR(Callback):
     def __init__(self,
+                 num_samples,
+                 batch_size,
                  max_lr,
                  end_percentage=0.1,
                  scale_percentage=None,
                  maximum_momentum=0.95,
-                 minimum_momentum=0.85,
-                 num_samples,
-                 batch_size,
+                 minimum_momentum=0.85,                
                  verbose=True):
         """ This callback implements a cyclical learning rate policy (CLR).
         This is a special case of Cyclic Learning Rates, where we have only 1 cycle.
